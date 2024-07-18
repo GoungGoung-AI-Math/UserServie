99f68dc4902fff7c7fc07cb3e92a3690747b4f15
makisekurisu <kurikame1017@gmail.com>
2024-07-18 11:28:13 +0900

    [Fix] 엔티티 클래스 매핑 및 오타 수정

--- src/main/java/User/Math/AI/domain/like/entity/Likes.java
+++ src/main/java/User/Math/AI/domain/like/entity/Likes.java
@@ -1,12 +1,10 @@
 package User.Math.AI.domain.like.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import User.Math.AI.domain.userProfile.entity.UserProfile;
+import jakarta.persistence.*;
 
 @Entity
-public class Like {
+public class Likes {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -14,4 +12,8 @@ public class Like {
 
     // ex 질문 아이디, 답변 아이디, 풀이 아이디 ...
     private long relationId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_profile_id")
+    private UserProfile userProfile;
 }
