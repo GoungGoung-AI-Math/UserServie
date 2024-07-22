540b0334650c269ce1a51af3ca2f8dfe186ead52
ohchansol <haxr369@gmail.com>
2024-07-22 10:05:00 +0900

    Feat : Kafka로 비동기 이벤트 전달 성공

--- kafka/producer/KafkaProducerImpl.java
+++ kafka/producer/KafkaProducerImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.my.kafka.infra.kafka.producer;
 import com.example.demo.my.kafka.infra.kafka.producer.exception.KafkaProducerException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -11,6 +12,7 @@ import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 import javax.annotation.PreDestroy;
 import java.io.Serializable;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Component
@@ -23,11 +25,26 @@ public class KafkaProducerImpl<K extends Serializable, V extends SpecificRecordB
     }
 
     @Override
-    public void send(String topicName, K key, V message, ListenableFutureCallback<SendResult<K, V>> callback) {
+    public void send(String topicName, K key, V message) {
         log.info("Sending message={} to topic={}", message, topicName);
         try {
-            ListenableFuture<SendResult<K, V>> kafkaResultFuture = (ListenableFuture<SendResult<K, V>>) kafkaTemplate.send(topicName, key, message);
-            kafkaResultFuture.addCallback(callback);
+            CompletableFuture<SendResult<K, V>> kafkaResultFuture = kafkaTemplate.send(topicName, key, message);
+            kafkaResultFuture.whenComplete((result, throwAble) -> {
+                        if(throwAble == null) {
+                            RecordMetadata metadata = result.getRecordMetadata();
+                            log.info("Received successful response from Kafka for id: {}" +
+                                            " Topic: {} Partition: {} Offset: {} Timestamp: {}",
+                                    key,
+                                    metadata.topic(),
+                                    metadata.partition(),
+                                    metadata.offset(),
+                                    metadata.timestamp());
+                        } else {
+                            log.error("Error while sending " + key +
+                                    " message {} to topic {}", message.toString(), topicName, throwAble);
+                            throw new RuntimeException(throwAble);
+                        }
+                    });
         } catch (KafkaException e) {
             log.error("Error on kafka producer with key: {}, message: {} and exception: {}", key, message,
                     e.getMessage());
