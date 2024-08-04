10a4143b63ffdbfca00503aaf39d0d131f7f34a4
makisekurisu <kurikame1017@gmail.com>
2024-08-04 09:31:10 +0900

    [Test] 문제에 해당하는 제출 결과 리스트 보낼 때 필요한 닉네임 문제 서버로 응답 카프카 배포 환경에서 테스트

--- src/main/java/User/Math/AI/domain/userProfile/kafka/listener/UserUpdateEventConsumer.java
+++ src/main/java/User/Math/AI/domain/userProfile/kafka/listener/UserUpdateEventConsumer.java
@@ -14,11 +14,11 @@ import org.springframework.stereotype.Service;
 public class UserUpdateEventConsumer {
     private final UserProfileService userProfileService;
 
-    @KafkaListener(topics = "user-update-topic", groupId = "user-update-group")
-    public void consume(UserUpdateEvent event) {
-        log.info("Received UserUpdateEvent.avsc: {}", event);
-        userProfileService.updateUserStatus(event);
-    }
+//    @KafkaListener(topics = "user-update-topic", groupId = "user-update-group")
+//    public void consume(UserUpdateEvent event) {
+//        log.info("Received UserUpdateEvent.avsc: {}", event);
+//        userProfileService.updateUserStatus(event);
+//    }
 
     @KafkaListener(topics = "user-profile-question-update-topic", groupId = "user-update-group")
     public void consume(UserProfileQuestionUpdateEvent event) {
